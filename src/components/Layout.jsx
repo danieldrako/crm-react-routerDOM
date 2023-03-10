e38a888f21d8a35e3e4a6f639c47a1f0bbbe1d5b
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-10 13:54:31 -0600

    after navigate between components with link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, NavLink, Link } from "react-router-dom"
 
 const Layout = () => {
   return (
     <div className="md:flex md:min-h-scren">
         <aside className="md:w-1/4 bg-blue-800 px-5 py-10">
-          <h2 className= "text-4xl font-black text-center text-white">Crm-Clientes</h2>
+          <h2 className= "text-4xl font-black text-center text-white">CRM - Clientes</h2>
+
+          <nav className="mt-10">
+              <Link className="text-2xl block mt-2 hover:text-blue-300 " to="/">Clientes</Link>
+              <Link className="text-2xl block mt-2 hover:text-blue-300 " to="/clientes/nuevo">Nuevo Cliente</Link>
+          </nav>
 
         </aside>
 
