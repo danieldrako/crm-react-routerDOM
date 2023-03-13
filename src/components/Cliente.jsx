629ac46837f7c2b9e6b449469f9a3b55c9ff1fa7
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-12 21:45:29 -0600

    after showing the rest of the informartion

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -2,13 +2,33 @@
 function Cliente({cliente}) {
 
     const {nombre, empresa, email, telefono, id} = cliente
+
     return (
-        <tr>
-            <td className='text-white p-6'>
-                {nombre}
+        <tr className="border-b">
+            <td className='text-white p-6 space-y-2'>
+                <p className="text-2xl">{nombre}</p>
+                <p>{empresa}</p>
             </td>
+
             <td className='text-white p-6'>
-                {empresa}
+                <p className=""> <span className=" uppercase font-bold">Email: </span>{email}</p>
+                <p className=""> <span className=" uppercase font-bold">Tel: </span>{telefono}</p>
+            </td>
+
+            <td className='text-white p-6 flex gap-3'>
+                <button
+                    type="button"
+                    className=" text-yellow-300 hover:text-amber-400 font-bold text-xs"
+                >
+                    Editar
+                </button>
+
+                <button
+                    type="button"
+                    className=" text-red-300 hover:text-red-500 font-bold text-xs"
+                >
+                    Eliminar
+                </button>
             </td>
         </tr>
     )
