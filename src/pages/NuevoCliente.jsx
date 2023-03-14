bafe136645db1d645ef609be91bd9842a226d425
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-14 09:32:31 -0600

    after npm i JSON-server

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -4,16 +4,19 @@ import Formulario from "../components/Formulario"
 
 export async function action({request}){
   const formData = await request.formData()
-  
   const datos = Object.fromEntries(formData)
+  const email = formData.get('email')
 
-  
   //*Validacion
   const errores = []
   if(Object.values(datos).includes('')){
     errores.push('Todos los campos son obligatorios')
   }
 
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)) {
+    errores.push('El Email no es válido')
+  }
 
   //*Retornar errores
   if(Object.keys(errores).length){
@@ -48,6 +51,7 @@ const NuevoCliente = () => {
 
         <Form
           method='post'
+          noValidate
         >
           <Formulario/>
 
